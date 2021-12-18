1633d6a40d7ee3668b18744bc1fd97036069e08d
Tubaias <topias_t@hotmail.com>
2021-12-18 22:37:58 +0200

    slow

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -22,7 +22,7 @@ static inline void check(cudaError_t err, const char* context) {
 
 #define CHECK(x) check(x, #x)
 
-__global__ void matrixMul(int ny, int nx, float* norData, float* result) {
+__global__ void matrixMul2(int ny, int nx, float* norData, float* result) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if (i >= ny || j > i) { return; }
@@ -34,16 +34,41 @@ __global__ void matrixMul(int ny, int nx, float* norData, float* result) {
     }
 }
 
-/*
-This is the function you need to implement. Quick reference:
-- input rows: 0 <= y < ny
-- input columns: 0 <= x < nx
-- element at row y and column x is stored in data[x + y*nx]
-- correlation between rows i and row j has to be stored in result[i + j*ny]
-- only parts with 0 <= j <= i < ny need to be filled
-*/
+__global__ void matrixMul(int ny, int nx, int nny, float* norData, float* result) {
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
+
+    for (int k = 0; k < nx; k++) {
+        float x[8];
+        float y[8];
+
+        for (int ib = 0; ib < 8; ++ib) {
+            int i = ic*64 + ib*8 + ia;
+            x[ib] = norData[i*nx + k];
+        }
+
+        for (int jb = 0; jb < 8; ++jb) {
+            int j = jc*64 + jb*8 + ja;
+            y[jb] = norData[j*nx + k];
+        }
+
+        for (int ib = 0; ib < 8; ++ib) {
+            for (int jb = 0; jb < 8; ++jb) {
+                int i = ic*64 + ib*8 + ia;
+                int j = jc*64 + jb*8 + ja;
+                if (i < ny && j <= i) {
+                    result[j*ny + i] += x[ib] * y[jb];
+                }
+            }
+        }
+    }
+}
+
 void correlate(int ny, int nx, const float *data, float *result) {
-    float* norData = new float[ny*nx];
+    int nny = roundup(ny, 64);
+    float* norData = new float[nny*nx]{0};
     
     // normalization
     for (int row = 0; row < ny; row++) {
@@ -55,7 +80,6 @@ void correlate(int ny, int nx, const float *data, float *result) {
         }
 
         float mean = sum / nx;
-
         for (int col = 0; col < nx; col++) {
             float normal = data[row*nx + col] - mean;
             norData[row*nx + col] = normal;
@@ -63,32 +87,27 @@ void correlate(int ny, int nx, const float *data, float *result) {
         }
 
         float sumsqrt = sqrt(sqsum);
-
         for (int col = 0; col < nx; col++) {
-            float buzz = norData[row*nx + col] / sumsqrt;
-            norData[row*nx + col] = buzz;
+            norData[row*nx + col] = norData[row*nx + col] / sumsqrt;
         }
     }
 
     float* norDataGPU = NULL;
-    CHECK(cudaMalloc((void**)&norDataGPU, ny * nx * sizeof(float)));
-    CHECK(cudaMemcpy(norDataGPU, norData, ny * nx * sizeof(float), cudaMemcpyHostToDevice));
-
+    CHECK(cudaMalloc((void**)&norDataGPU, nny * nx * sizeof(float)));
+    CHECK(cudaMemcpy(norDataGPU, norData, nny * nx * sizeof(float), cudaMemcpyHostToDevice));
     float* resultGPU = NULL;
     CHECK(cudaMalloc((void**)&resultGPU, ny * ny * sizeof(float)));
     CHECK(cudaMemcpy(resultGPU, result, ny * ny * sizeof(float), cudaMemcpyHostToDevice));
 
     // matrix multiplication
-    dim3 dimBlock(16, 16);
-    dim3 dimGrid(divup(ny, dimBlock.x), divup(ny, dimBlock.y));
-    matrixMul<<<dimGrid, dimBlock>>>(ny, nx, norDataGPU, resultGPU);
+    dim3 dimBlock(8, 8);
+    dim3 dimGrid(nny / 64, nny / 64);
+    matrixMul<<<dimGrid, dimBlock>>>(ny, nx, nny, norDataGPU, resultGPU);
     CHECK(cudaGetLastError());
 
-    // results
+    // finish
     CHECK(cudaMemcpy(result, resultGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost));
-
     CHECK(cudaFree(norDataGPU));
     CHECK(cudaFree(resultGPU));
-
     delete[] norData;
 }
